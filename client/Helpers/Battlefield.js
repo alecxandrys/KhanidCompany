94ae1353c5fc6675c3ca261b6c0aa6b8d4e2a7f6
Alecxandr <alecxandrys@users.noreply.github.com>
2015-11-12 21:56:18 +0300

    Start phaser.io break the redirect

--- client/Helpers/Battlefield.js
+++ client/Helpers/Battlefield.js
@@ -2,8 +2,24 @@
  * Created by Alecxandrys on 10.11.2015.
  * We need phaser.io here!!!
  */
-Template.Battlefield.helpers({
 
+var game;
 
+function update() {
 
+}
+
+function create() {
+    game.add.sprite(0,0,'Grass');
+}
+
+function preload() {
+    game.load.image('Grass', 'BattleResource/Grass.png');
+}
+
+Template.Battlefield.onRendered(function(){
+
+});
+Template.Battlefield.onCreated(function(){
+    game = new Phaser.Game(800, 600, Phaser.AUTO, 'field', { preload: preload, create: create, update: update });
 });
