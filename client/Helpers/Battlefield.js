3adc3abe9e3b004b0447b9a393bb2d480cdd2209
Alecxandr <alecxandrys@users.noreply.github.com>
2015-11-17 19:11:02 +0300

    Scale factor

--- client/Helpers/Battlefield.js
+++ client/Helpers/Battlefield.js
@@ -8,8 +8,10 @@ var game;
 BS={};
 var h;
 var w;
-
-
+/**
+ * Basic image height=80
+ * Basic image width=60
+ */
 function preload() {
     game.load.image('Grass', 'BattleResource/Grass.png');
     game.load.image('Cover', 'BattleResource/Cover.png');
@@ -17,31 +19,60 @@ function preload() {
     game.load.image('Diff', 'BattleResource/Diff.png');
     game.load.image('Unreached', 'BattleResource/Unreached.png');
 }
-
-function create() {
-    var x = 20;
-    var y = 12;
-
-    var xStep = Math.floor(w / x);
-    var yStep = Math.floor(h / y);
-
-    var tiles = game.add.group();
-
-
-    for(var i=0;i<y;i++)
+/**
+ * Scale cell here
+ * it's a multiple point, can be float.
+ * Ideal width is 60*20.5=1230;
+ * Ideal height is 80*12=960;
+ * h/1230=x scale factor;
+ * w/960=y scale factor;
+ */
+function create()
     {
-        for(var j=0;j<x;j++) {
-            var tmp=BS.map[i][j].ground;
-            var cell;
+        var x = 20;
+        var y = 12;
 
-            if(tmp==1){cell=tiles.create(xStep*(j),yStep*(i),'Grass');}
-            else if(tmp==2){cell=tiles.create(xStep*(j),yStep*(i),'Cover');}
-            else if(tmp==3){cell=tiles.create(xStep*(j),yStep*(i),'Danger');}
-            else if(tmp==4){cell=tiles.create(xStep*(j),yStep*(i),'Diff');}
-            else if(tmp==0){cell=tiles.create(xStep*(j),yStep*(i),'Unreached');}
-        }
+        var xStep = Math.floor(w / (x + 0.5));
+        var yStep = Math.floor(h / (y + 0.5));
+
+        var tiles = game.add.group();
+
+        var xScale=h/1230;
+        var yScale=w/960;
+
+        for(var i = 0; i < y; i++)
+            {
+                var shift = i % 2;
+                for(var j = 0; j < x; j++)
+                    {
+                        var tmp = BS.map[i][j].ground;
+                        var cell;
+                        if(tmp == 1)
+                            {
+                                cell = tiles.create(Math.floor(xStep * (j + 0.5 * shift)), yStep * (i), 'Grass');
+                            }
+                        else if(tmp == 2)
+                            {
+                                cell = tiles.create(Math.floor(xStep * (j + 0.5 * shift)), yStep * (i), 'Cover');
+                            }
+                        else if(tmp == 3)
+                            {
+                                cell = tiles.create(Math.floor(xStep * (j + 0.5 * shift)), yStep * (i), 'Danger');
+                            }
+                        else if(tmp == 4)
+                            {
+                                cell = tiles.create(Math.floor(xStep * (j + 0.5 * shift)), yStep * (i), 'Diff');
+                            }
+                        else if(tmp == 0)
+                            {
+                                cell = tiles.create(Math.floor(xStep * (j + 0.5 * shift)), yStep * (i), 'Unreached');
+                            }
+
+                        //scale itself
+                        cell.scale.setTo(xScale,yScale);
+                    }
+            }
     }
-}
 
 
 function update() {
@@ -51,9 +82,8 @@ function update() {
 
 Template.Battlefield.onRendered(function(){
 
-    game = new Phaser.Game(w, h, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-
-   });
+    game = new Phaser.Game(w, h, Phaser.AUTO, 'field', { preload: preload, create: create, update: update });
+    });
 
 
 /**
