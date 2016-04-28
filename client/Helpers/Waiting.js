95888d64446759e9f42d05e43dc93479525f302a
Alecxandr <alecxandrys@users.noreply.github.com>
2016-04-28 14:41:34 +0300

    covered <a> to redirect changed to Template.wait.autorun (untested)

--- client/Helpers/Waiting.js
+++ client/Helpers/Waiting.js
@@ -8,27 +8,27 @@ Template.wait.helpers({
     players: function()
         {
             return readyPlayers.find({});
-        },
-    /**
-     * redirection
-     * be careful!
-     * very dangerous paramstring
-     * it's only success way to take param.
-     * @returns {string}
-     */
-    battleID: function()
+        }
+
+});
+/**
+ * redirection
+ * be careful!
+ * very dangerous paramstring
+ * it's only success way to take param.
+ * @returns {string}
+ */
+Template.wait.autorun(function(){
+    var x = battles.find({});
+    var y = battles.findOne({});
+    if(x.count() === 1)
         {
-            var x = battles.find({});
-            var y = battles.findOne({});
-            if(x.count() === 1)
-                {
-                    var paramstring = "name1=" + y.name1 + "&name2=" + y.name2 + "&battleID=" + y.battleID;
-                    Router.go('battlefield', {}, {query: paramstring});
-                }
-            else if(x.count() !== 1)
-                {
-                    console.log('Error:Balancer twice push player. Please, wait');
-                    return ("Waiting " + x.count() + " " + readyPlayers.find().count());
-                }
+            var paramstring = "name1=" + y.name1 + "&name2=" + y.name2 + "&battleID=" + y.battleID;
+            Router.go('battlefield', {}, {query: paramstring});
+        }
+    else if(x.count() !== 1)
+        {
+            console.log('Error:Balancer twice push player. Please, wait');
+            return ("Waiting " + x.count() + " " + readyPlayers.find().count());
         }
 });
