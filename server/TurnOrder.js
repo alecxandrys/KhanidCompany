df35a7674d02557f806969cd69130a6ac72ebe0e
alecxandrys <alecxandrys@gmail.com>
2017-02-15 12:08:47 +0300

    TurnOrder realized and right import in Balancer

--- server/TurnOrder.js
+++ server/TurnOrder.js
@@ -1,23 +1,22 @@
 /**
  * Created by Пользователь on 30.05.2016.
+ * impossible pass function throw MongoDB (and JSON)
+ * Have to create orderLine instantly and make some func, which change state
  */
+export {TurnOrder}
 function TurnOrder(BS)
 {
-    for (let card in BS.deck1)
+    for(let card in BS.deck1)
     {
-        card.startATB=Math.floor(Math.random() * 11);
+        card.startATB=Math.floor(Math.random()*11);
     }
-    for (let card in BS.deck2)
+    for(let card in BS.deck2)
     {
-        card.startATB=Math.floor(Math.random() * 11);
+        card.startATB=Math.floor(Math.random()*11);
     }
 
     this.orderLine=BS.deck1+BS.deck2;
 
-    this.Change=function()
-    {
-
-    };
-
     return this;
-};
+}
+
